279cd4d37279664fdaaa85afe109272b7b4d468b
Henrik Zimmermann <henrik@zi-data.com>
2022-01-15 00:46:18 -0500

    Switched to mysql2 from mysql.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import bodyParser from "body-parser"
-import mysql from "mysql"
+import mysql2, { OkPacket, RowDataPacket } from "mysql2"
 import dotenv from "dotenv"
 import path from "path"
 import winston from "winston"
@@ -18,34 +18,7 @@ type ErrorResponse = {
 }
 
 
-function prepareUpdateStatement(body: any, id: number): string | null {
-    let firstValue = true
-    let updatePairs = ""
-    for (const key in body) {
-        if (key === "id") {
-            continue
-        }
-
-        if (firstValue) {
-            firstValue = false
-        } else {
-            updatePairs += ", "
-        }
-
-        updatePairs += `${mysql.escapeId(key)} = ${mysql.escape(body[key])}`
-    }
-
-    if (!firstValue) {
-        // There has been at least one value to update
-
-        return `UPDATE inventory SET ${updatePairs} WHERE id = ${mysql.escape(id)}`
-    }
-
-    return null
-}
-
-
-function logDbError(error: mysql.MysqlError, hostname?: string) {
+function logDbError(error: mysql2.QueryError, hostname?: string) {
     if (hostname) {
         logger.error(`${hostname} caused database error ${error.code}: ${error.message}`)
     } else {
@@ -69,7 +42,7 @@ const logger = winston.createLogger({
     ]
 })
 
-const dbConnConfig: mysql.ConnectionConfig  = {
+const dbConnConfig: mysql2.ConnectionOptions = {
     host: process.env.DB_HOST,
     port: Number.parseInt(process.env.DB_PORT, 10),
     user: process.env.DB_USER,
@@ -77,9 +50,9 @@ const dbConnConfig: mysql.ConnectionConfig  = {
     database: process.env.DB_NAME
 }
 
-const dbConnection = mysql.createConnection(dbConnConfig)
+const dbConnection = mysql2.createConnection(dbConnConfig)
 
-dbConnection.connect((err, args) => {
+dbConnection.connect(err => {
     if (!err) {
         logger.info(`Connected to database ${dbConnConfig.database} `
         + `on ${dbConnConfig.host}:${dbConnConfig.port} `
@@ -156,8 +129,8 @@ app.get("/inventory/deleted", (req, res) => {
 })
 
 app.get("/inventory/item/:id", (req, res) => {
-    dbConnection.query(`SELECT * FROM inventory WHERE id = ${mysql.escape(req.params.id)}`,
-    (error, results, fields) => {
+    dbConnection.query(`SELECT * FROM inventory WHERE id = ${mysql2.escape(req.params.id)}`,
+    (error, results: mysql2.RowDataPacket, fields) => {
         if (!error) {
             logger.info(`${req.hostname} requested entry with id ${req.params.id}`)
 
@@ -174,11 +147,11 @@ app.get("/inventory/item/:id", (req, res) => {
 app.put("/inventory", (req, res) => {
     if (req.body.hasOwnProperty("name") && req.body.hasOwnProperty("count")
         && req.body.name.length !== 0 && isInteger(req.body.count) && req.body.count >= 0) {
-        const name = mysql.escape(req.body.name)
-        const count = mysql.escape(req.body.count)
+        const name = mysql2.escape(req.body.name)
+        const count = mysql2.escape(req.body.count)
 
         dbConnection.query(`INSERT INTO inventory (name, count) VALUES (${name}, ${count})`,
-            (error, results, fields) => {
+            (error, results: mysql2.OkPacket, fields) => {
             if (!error) {
                 logger.info(`${req.hostname} created entry with id ${results.insertId} in inventory`)
 
@@ -220,11 +193,11 @@ app.put("/inventory/item/:id", (req, res) => {
     }
 
     const id = Number.parseInt(req.params.id, 10)
-    const updateStmt = prepareUpdateStatement(req.body, id)
-    if (updateStmt) {
+    if (req.body.hasOwnProperty("name") || req.body.hasOwnProperty("count")) {
         // Request to update
 
-        dbConnection.query(updateStmt, (error, results, fields) => {
+        dbConnection.query(`UPDATE inventory SET ? WHERE id = ${mysql2.escape(id)}`,
+        req.body, (error, results: mysql2.OkPacket, fields) => {
             if (!error) {
                 if (results.affectedRows > 0) {
                     logger.info(`${req.hostname} updated entry with id ${id}`)
@@ -245,17 +218,17 @@ app.put("/inventory/item/:id", (req, res) => {
         // Request to restore
 
         dbConnection.query(`SELECT deletion_id FROM inventory `
-            + `WHERE id = ${mysql.escape(id)} AND deletion_id IS NOT NULL`,
-            (error, results, fields) => {
+            + `WHERE id = ${mysql2.escape(id)} AND deletion_id IS NOT NULL`,
+            (error, results: mysql2.RowDataPacket, fields) => {
                 if (!error) {
                     if (results.length === 1) {
                         const deletionId = results[0].deletion_id
 
                         dbConnection.query(`UPDATE inventory SET deletion_id = NULL `
-                        + `WHERE id = ${mysql.escape(id)}`,
+                        + `WHERE id = ${mysql2.escape(id)}`,
                         (error2, results2, fields2) => {
                             if (!error2) {
-                                dbConnection.query(`DELETE FROM deletions WHERE id = ${mysql.escape(deletionId)}`,
+                                dbConnection.query(`DELETE FROM deletions WHERE id = ${mysql2.escape(deletionId)}`,
                                 (error3, results3, fields3) => {
                                     if (!error3) {
                                         res.send()
@@ -299,20 +272,20 @@ app.delete("/inventory/item/:id", (req, res) => {
 
     if (req.body.hasOwnProperty("comment")) {
         // TODO: Handle if entry already marked for deletion.
-        const comment = mysql.escape(req.body.comment)
+        const comment = mysql2.escape(req.body.comment)
 
         const stmt = `INSERT INTO deletions (comment) VALUES (${comment})`
 
         dbConnection.query(stmt,
-        (error, results, fields) => {
+        (error, results: mysql2.OkPacket, fields) => {
             if (!error) {
-                const deletionId = mysql.escape(results.insertId)
+                const deletionId = mysql2.escape(results.insertId)
                 const id = Number.parseInt(req.params.id, 10)
 
                 logger.info(`${req.hostname} added a deletion comment for entry with id ${id} in inventory`)
 
-                const updateStmt = prepareUpdateStatement({ deletion_id: deletionId }, id)
-                dbConnection.query(updateStmt, (error2, results2, fields2) => {
+                dbConnection.query(`UPDATE inventory SET ? WHERE id = ${mysql2.escape(id)}`,
+                { deletion_id: deletionId }, (error2, results2, fields2) => {
                     if (!error2) {
                         logger.info(`${req.hostname} marked entry with id ${id} in inventory as deleted`)
 
@@ -336,7 +309,7 @@ app.delete("/inventory/item/:id", (req, res) => {
 
 app.use(express.static(path.resolve(__dirname, "..", "public")))
 
-// TODO only start web server when database connection is established
+
 const port = process.env.PORT
 app.listen(port, () => {
     logger.info(`Started web server listening on port ${port}`)
