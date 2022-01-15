b0beeb9dc326f99a5425f45f1ffca12d5221dacc
Henrik Zimmermann <henrik@zi-data.com>
2022-01-15 12:37:40 -0500

    Promisified entry deletion.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express from "express"
 import bodyParser from "body-parser"
-import mysql2, { OkPacket, RowDataPacket } from "mysql2"
+import mysql2, { OkPacket, QueryError, RowDataPacket } from "mysql2"
 import dotenv from "dotenv"
 import path from "path"
 import winston from "winston"
+import { MysqlError } from "mysql"
 
 
 enum Error {
@@ -42,53 +43,29 @@ const logger = winston.createLogger({
     ]
 })
 
-const dbConnConfig: mysql2.ConnectionOptions = {
-    host: process.env.DB_HOST,
-    port: Number.parseInt(process.env.DB_PORT, 10),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+
+function isMysqlError(error: ErrorResponse | mysql2.QueryError): error is mysql2.QueryError {
+    return Boolean((error as mysql2.QueryError).name)
 }
 
-const dbConnection = mysql2.createConnection(dbConnConfig)
+function hasValidId(req: express.Request, res: express.Response, action: string = "make changes") {
+    return new Promise((resolve, reject) => {
+        if (isInteger(req.params.id)) {
+            resolve(0)
+        } else {
+            logger.info(`${req.hostname} tried to ${action} without a valid entry `
+            + `id (${req.params.id})`)
 
-dbConnection.connect(err => {
-    if (!err) {
-        logger.info(`Connected to database ${dbConnConfig.database} `
-        + `on ${dbConnConfig.host}:${dbConnConfig.port} `
-        + `as user ${dbConnConfig.user}`)
-    } else {
-        logDbError(err)
-    }
-})
+            const body: ErrorResponse = {
+                name: Error.FIELD,
+                message: "The id has to be specified as number in the url path"
+            }
 
-
-dbConnection.query(
-    "CREATE TABLE IF NOT EXISTS deletions "
-    + "(id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, "
-    + "created_at TIMESTAMP NOT NULL DEFAULT NOW(), "
-    + "update_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(), "
-    + "comment VARCHAR(255) NOT NULL)",
-    (error, results, fields) => {
-        if (error) {
-            logDbError(error)
+            res.status(400)
+            reject(body)
         }
     })
-
-dbConnection.query(
-    "CREATE TABLE IF NOT EXISTS inventory "
-    + "(id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, "
-    + "deletion_id BIGINT, "
-    + "created_at TIMESTAMP NOT NULL DEFAULT NOW(), "
-    + "updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(), "
-    + "name VARCHAR(64) NOT NULL, "
-    + "count INT NOT NULL DEFAULT 0, "
-    + "FOREIGN KEY (deletion_id) REFERENCES deletions (id))",
-    (error, results, fields) => {
-        if (error) {
-            logDbError(error)
-        }
-    })
+}
 
 
 const app = express()
@@ -179,7 +156,7 @@ app.put("/inventory", (req, res) => {
     }
 })
 
-app.put("/inventory/item/:id", (req, res) => {
+app.put("/inventory/item/:id", (req, res) => {    
     if (!isInteger(req.params.id)) {
         logger.info(`${req.hostname} tried to update without a valid entry id (${req.params.id})`)
 
@@ -258,59 +235,106 @@ app.put("/inventory/item/:id", (req, res) => {
 })
 
 app.delete("/inventory/item/:id", (req, res) => {
-    if (!isInteger(req.params.id)) {
-        logger.info(`${req.hostname} tried to delete without a valid entry id (${req.params.id})`)
-
-        const body: ErrorResponse = {
-            name: Error.FIELD,
-            message: "The id has to be specified as number in the url path"
-        }
-
-        res.status(400).send(body)
-        return
-    }
-
-    if (req.body.hasOwnProperty("comment")) {
-        // TODO: Handle if entry already marked for deletion.
-        const comment = mysql2.escape(req.body.comment)
-
-        const stmt = `INSERT INTO deletions (comment) VALUES (${comment})`
-
-        dbConnection.query(stmt,
-        (error, results: mysql2.OkPacket, fields) => {
-            if (!error) {
-                const deletionId = mysql2.escape(results.insertId)
-                const id = Number.parseInt(req.params.id, 10)
-
-                logger.info(`${req.hostname} added a deletion comment for entry with id ${id} in inventory`)
-
-                dbConnection.query(`UPDATE inventory SET ? WHERE id = ${mysql2.escape(id)}`,
-                { deletion_id: deletionId }, (error2, results2, fields2) => {
-                    if (!error2) {
-                        logger.info(`${req.hostname} marked entry with id ${id} in inventory as deleted`)
-
-                        res.send()
-                    } else {
-                        logDbError(error2, req.hostname)
-
-                        const body: ErrorResponse = { name: Error.DB }
-                        res.status(500).send(body)
-                    }
-                })
+    hasValidId(req, res, "delete")
+    .then(() => {
+        return new Promise((resolve, reject) => {
+            if (req.body.hasOwnProperty("comment")) {
+                resolve(mysql2.escape(req.body.comment))
             } else {
-                logDbError(error, req.hostname)
+                logger.info(`${req.hostname} requested to delete entry in `
+                + `inventory without a deletion comment`)
 
-                const body: ErrorResponse = { name: Error.DB }
-                res.status(500).send(body)
+                const body: ErrorResponse = {
+                    name: Error.FIELD,
+                    message: "There has to be a deletion comment for a deletion"
+                }
+
+                res.status(400)
+                reject(body)
             }
         })
-    }
+    })
+    .then((comment) => {
+        const stmt = `INSERT INTO deletions (comment) VALUES (${comment})`
+        return dbPromise.query(stmt)
+    })
+    .then(([results, fields]) => {
+        logger.info(`${req.hostname} added a deletion comment for entry with `
+        + `id ${req.params.id} in inventory`)
+
+        results = results as OkPacket
+        const deletionId = mysql2.escape(results.insertId)
+        const id = Number.parseInt(req.params.id, 10)
+
+        const stmt = `UPDATE inventory SET ? WHERE id = ${mysql2.escape(id)}`
+
+        return dbPromise.query(stmt, { deletion_id: deletionId })
+    })
+    .then(() => {
+        logger.info(`${req.hostname} marked entry with id ${req.params.id} in `
+        + `inventory as deleted`)
+
+        res.send()
+    }, (error) => {
+        if (isMysqlError(error)) {
+            logDbError(error, req.hostname)
+
+            const body: ErrorResponse = { name: Error.DB }
+            res.status(500).send(body)
+        } else {
+            return res.send(error)
+        }
+    })
 })
 
 app.use(express.static(path.resolve(__dirname, "..", "public")))
 
 
-const port = process.env.PORT
-app.listen(port, () => {
-    logger.info(`Started web server listening on port ${port}`)
+const dbConnConfig: mysql2.ConnectionOptions = {
+    host: process.env.DB_HOST,
+    port: Number.parseInt(process.env.DB_PORT, 10),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+}
+
+const dbConnection = mysql2.createConnection(dbConnConfig)
+
+dbConnection.connect(err => {
+    if (!err) {
+        logger.info(`Connected to database ${dbConnConfig.database} `
+        + `on ${dbConnConfig.host}:${dbConnConfig.port} `
+        + `as user ${dbConnConfig.user}`)
+    } else {
+        logDbError(err)
+    }
 })
+
+const dbPromise = dbConnection.promise()
+dbPromise.query(
+    "CREATE TABLE IF NOT EXISTS deletions "
+    + "(id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, "
+    + "created_at TIMESTAMP NOT NULL DEFAULT NOW(), "
+    + "update_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(), "
+    + "comment VARCHAR(255) NOT NULL)")
+    .then(() => {
+        return dbPromise.query("CREATE TABLE IF NOT EXISTS inventory "
+        + "(id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, "
+        + "deletion_id BIGINT, "
+        + "created_at TIMESTAMP NOT NULL DEFAULT NOW(), "
+        + "updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(), "
+        + "name VARCHAR(64) NOT NULL, "
+        + "count INT NOT NULL DEFAULT 0, "
+        + "FOREIGN KEY (deletion_id) REFERENCES deletions (id))")
+    })
+    .then(() => {
+        return new Promise((resolve, reject) => {
+            const port = process.env.PORT
+            app.listen(port, () => {
+                logger.info(`Started web server listening on port ${port}`)
+            })
+
+            resolve(0)
+        })
+    })
+    .catch(logDbError)
