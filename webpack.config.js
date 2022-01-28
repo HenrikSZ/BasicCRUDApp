6feedecb6a78de786ff7f53064d4a256dbb0c2b0
Henrik Zimmermann <henrik@zi-data.com>
2022-01-28 14:07:27 -0500

    Added webpack, react and eslint as dependencies.

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve("dist"),
+    publicPath: "/",
+  },
+  module: {
+    rules:[
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.html$/,
+        use: "html-loader"
+      },
+      /*Choose only one of the following two: if you're using 
+      plain CSS, use the first one, and if you're using a
+      preprocessor, in this case SASS, use the second one*/
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use:[
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ],
+      },
+    ], 
+  },  
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "index.html"
+    }),
+  ]
+}
