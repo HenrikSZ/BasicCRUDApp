00ab74d02e2683cfa5aea30d66740929d1ba4001
Henrik Zimmermann <henrik@zi-data.com>
2022-02-13 16:59:56 -0500

    Added migrations for shipment tables / Modified counts to unsigned int.

---
+++ migrations/001.undo.create_shipment_tables.sql
@@ -0,0 +1,10 @@
+DROP TRIGGER items_count_too_low_update;
+DROP TRIGGER items_count_too_low_insert;
+
+DROP TRIGGER shipment_items_count_too_high_update;
+DROP TRIGGER shipment_items_count_too_high_insert;
+
+DROP PROCEDURE check_shipment_items_count;
+
+DROP TABLE shipments_to_items;
+DROP TABLE shipments;
